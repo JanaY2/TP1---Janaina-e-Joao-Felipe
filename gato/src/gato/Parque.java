e9a8f045aaff7c21df4f59319e87395b81f0b8f1
JanaY2 <fuiludibriada@gmail.com>
2019-06-06 10:31:35 -0300

    aaa

---
+++ gato/src/gato/Parque.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+/**
+ *
+ * @author aluno
+ */
+public class Parque {
+    public static int main(String[] args);
+     Gato duin=new Gato();
+      duin.none="Bola";
+      duin.Gato();
+     Gato gatinho=new Gato("duin");
+     gatinho.idade=2;
+     gatinho.macho=true;
+     gatinho.Gato();
+     duin.registro=gatinho;
+     return 0;
+}
+     
+             
+              
+             
+}
