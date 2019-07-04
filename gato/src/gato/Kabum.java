b8d9dbfedfb1a67e5de730021f814ecc92e66e1f
JanaY2 <fuiludibriada@gmail.com>
2019-07-04 10:00:16 -0300

    aloalo

---
+++ gato/src/gato/Kabum.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+/**
+ *
+ * @author Janaina
+ */
+public class Kabum {
+        private String email;
+        private String primeironome;
+        private String sobrenome;
+        private String cpf;
+        private String rg;
+        private String datadenascimento;
+        private String sexo;
+        private String seutelefones;
+
+        /**
+         * @return the email
+         */
+        public String getEmail() {
+            return email;
+        }
+
+        /**
+         * @param email the email to set
+         */
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        /**
+         * @return the primeironome
+         */
+        public String getPrimeironome() {
+            return primeironome;
+        }
+
+        /**
+         * @param primeironome the primeironome to set
+         */
+        public void setPrimeironome(String primeironome) {
+            this.primeironome = primeironome;
+        }
+
+        /**
+         * @return the sobrenome
+         */
+        public String getSobrenome() {
+            return sobrenome;
+        }
+
+        /**
+         * @param sobrenome the sobrenome to set
+         */
+        public void setSobrenome(String sobrenome) {
+            this.sobrenome = sobrenome;
+        }
+
+        /**
+         * @return the cpf
+         */
+        public String getCpf() {
+            return cpf;
+        }
+
+        /**
+         * @param cpf the cpf to set
+         */
+        public void setCpf(String cpf) {
+            this.cpf = cpf;
+        }
+
+        /**
+         * @return the rg
+         */
+        public String getRg() {
+            return rg;
+        }
+
+        /**
+         * @param rg the rg to set
+         */
+        public void setRg(String rg) {
+            this.rg = rg;
+        }
+
+        /**
+         * @return the datadenascimento
+         */
+        public String getDatadenascimento() {
+            return datadenascimento;
+        }
+
+        /**
+         * @param datadenascimento the datadenascimento to set
+         */
+        public void setDatadenascimento(String datadenascimento) {
+            this.datadenascimento = datadenascimento;
+        }
+
+        /**
+         * @return the sexo
+         */
+        public String getSexo() {
+            return sexo;
+        }
+
+        /**
+         * @param sexo the sexo to set
+         */
+        public void setSexo(String sexo) {
+            this.sexo = sexo;
+        }
+
+        /**
+         * @return the seutelefones
+         */
+        public String getSeutelefones() {
+            return seutelefones;
+        }
+
+        /**
+         * @param seutelefones the seutelefones to set
+         */
+        public void setSeutelefones(String seutelefones) {
+            this.seutelefones = seutelefones;
+        }
+}
